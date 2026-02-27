cb2599e90fc2c501c33f4c2b9b70dacbca1032af
N. Escobar <nick@nickesc.io>
2026-02-26 22:27:52 -0500

    update badges

--- src/badges.ts
+++ src/badges.ts
@@ -10,17 +10,12 @@ function svgFileToBase64(filename: string) {
     return `data:image/svg+xml;base64,${btoa(fs.readFileSync(path.join(process.cwd(), ASSET_DIR, filename), "utf8"))}`;
 }
 
-function iconToBase64(icon: SimpleIcon) {
-    const svg = icon.svg.replace("<svg", '<svg fill="#fff"');
+function iconToBase64(icon: SimpleIcon, color: "light" | "dark" = "light") {
+    const svg = icon.svg.replace("<svg", `<svg fill="${color === "light" ? "#fff" : "#333"}"`);
     return `data:image/svg+xml;base64,${btoa(svg)}`;
 }
 
 export const LanguageBadges: Format[] = [
-    {
-        message: "React",
-        color: "#61dafb",
-        logoBase64: iconToBase64(icons.siReact),
-    },
     {
         message: "TypeScript",
         color: "#3178C6",
@@ -28,8 +23,8 @@ export const LanguageBadges: Format[] = [
     },
     {
         message: "JavaScript",
-        color: "#F1DD35",
-        logoBase64: iconToBase64(icons.siJavascript),
+        color: "#f0db4f",
+        logoBase64: iconToBase64(icons.siJavascript, "dark"),
     },
     {
         message: "HTML",
@@ -42,6 +37,11 @@ export const LanguageBadges: Format[] = [
         logoBase64: iconToBase64(icons.siCss),
     },
     {
+        message: "React",
+        color: "#3eb6d6",
+        logoBase64: iconToBase64(icons.siReact),
+    },
+    {
         message: "Nodejs",
         color: "#339933",
         logoBase64: iconToBase64(icons.siNodedotjs),
@@ -49,7 +49,7 @@ export const LanguageBadges: Format[] = [
 
     {
         message: "Python",
-        color: "#4483b5",
+        color: "#306998",
         logoBase64: iconToBase64(icons.siPython),
     },
     {
@@ -57,37 +57,61 @@ export const LanguageBadges: Format[] = [
         color: "#111111",
         logoBase64: iconToBase64(icons.siZsh),
     },
+    {
+        message: "Godot",
+        color: "#478CBF",
+        logoBase64: iconToBase64(icons.siGodotengine),
+    },
 ];
 
 export const DistributionBadges: Format[] = [
     {
-        message: "GitHub",
-        color: "#101411",
+        label: "GitHub",
+        message: "Source code",
+        color: "hsl(135, 11%, 7%)",
+        labelColor: "hsl(135, 11%, 4%)",
         logoBase64: iconToBase64(icons.siGithub),
     },
     {
-        message: "NPM",
-        color: "#CC3534",
+        label: "NPM",
+        message: "Packages",
+        color: "hsl(0, 60%, 50%)",
+        labelColor: "hsl(0, 60%, 35%)",
         logoBase64: iconToBase64(icons.siNpm),
     },
     {
-        message: "Modrinth",
-        color: "#1BD96A",
+        label: "Modrinth",
+        message: "Minecraft",
+        color: "hsl(145, 78%, 48%)",
+        labelColor: "hsl(145, 78%, 33%)",
         logoBase64: iconToBase64(icons.siModrinth),
     },
     {
-        message: "VS Marketplace",
-        color: "#68217a",
+        label: "VS Marketplace",
+        message: "Extensions",
+        color: "hsl(288, 57%, 30%)",
+        labelColor: "hsl(288, 57%, 15%)",
         logoBase64: svgFileToBase64("vs-mp.svg"),
     },
     {
-        message: "OpenVSX",
-        color: "#a60ee5",
+        label: "OpenVSX",
+        message: "Extensions",
+        color: "hsl(282, 88%, 48%)",
+        labelColor: "hsl(282, 88%, 33%)",
         logoBase64: svgFileToBase64("open-vsx.svg"),
     },
     {
-        message: "Itch.io",
-        color: "#fa5c5c",
+        label: "Itch.io",
+        message: "Games",
+        color: "hsl(0, 94%, 67%)",
+        labelColor: "hsl(0, 94%, 52%)",
         logoBase64: iconToBase64(icons.siItchdotio),
+    },
+    {
+        label: "Firefox Add-ons",
+        message: "Add-ons",
+        color: "hsl(22, 100%, 67%)",
+        labelColor: "hsl(22, 100%, 52%)",
+        logoBase64: iconToBase64(icons.siFirefox),
     },
 ];
